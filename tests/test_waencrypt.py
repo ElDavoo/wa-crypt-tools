5c702fefc59bea3146ce3ed123d876ece50170be
David <github@davidepalma.it>
2023-08-25 08:38:49 +0200

    add test case and files for crypt14

--- tests/test_waencrypt.py
+++ tests/test_waencrypt.py
@@ -1,13 +1,14 @@
 import os
 import zlib
 
+from wa_crypt_tools.lib.db.db14 import Database14
 from wa_crypt_tools.lib.db.db15 import Database15
 from wa_crypt_tools.lib.key.keyfactory import KeyFactory
 from wa_crypt_tools.lib.props import Props
 from hashlib import sha512
 
-class Test_Encryption15:
-    def test_main(self):
+class Test_Encryption:
+    def test_encryption15(self):
         key = KeyFactory.new("res/encrypted_backup.key")
         props = Props(wa_version="2.22.5.13", jid="67", features=[5,7,8,13,14,19,22,25,28,30,31,32,36,37], max_feature=37)
         db = Database15(key=key, iv=bytes.fromhex("C395EE009CF8B68AC0EA760550F6559C"))
@@ -19,4 +20,14 @@ class Test_Encryption15:
             orig_check = sha512(f.read()).digest()
         assert new_check == orig_check
 
-
+    def test_encryption14(self):
+        key = KeyFactory.new("res/key")
+        props = Props(wa_version="2.22.5.13", jid="67", features=[5,7,8,13,14,19,22,25,28,30,31,32,36,37], max_feature=37)
+        db = Database14(key=key, iv=bytes.fromhex("EA53CEAE36ECAB50BC331AEB62491625"))
+        data = db.encrypt(key, props, zlib.compress(open("res/msgstore.db", 'rb').read(), 1, 15))
+        new_check = sha512(data).digest()
+        with open("res/msgstore-new.db.crypt14", 'wb') as f:
+            f.write(data)
+        with open("res/msgstore.db.crypt14", 'rb') as f:
+            orig_check = sha512(f.read()).digest()
+        assert new_check == orig_check
