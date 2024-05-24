92fd0166e57f97114fa5e521204038498aa050cd
Davide Palma (ElDavo) <github@davidepalma.it>
2024-05-24 19:18:04 +0200

    Regen protobuf files

--- src/wa_crypt_tools/proto/backup_prefix_pb2.py
+++ src/wa_crypt_tools/proto/backup_prefix_pb2.py
@@ -1,8 +1,10 @@
 """Generated protocol buffer code."""
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import descriptor_pool as _descriptor_pool
+from google.protobuf import runtime_version as _runtime_version
 from google.protobuf import symbol_database as _symbol_database
 from google.protobuf.internal import builder as _builder
+_runtime_version.ValidateProtobufRuntimeVersion(_runtime_version.Domain.PUBLIC, 5, 27, 0, '', 'backup_prefix.proto')
 _sym_db = _symbol_database.Default()
 from . import C14_cipher_pb2 as C14__cipher__pb2
 from . import C15_IV_pb2 as C15__IV__pb2
