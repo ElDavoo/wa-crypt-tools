5e445c874e7470e558e127b33e9df1ab0e403541
Davide Palma (ElDavo) <github@davidepalma.it>
2024-12-14 12:46:08 +0100

    chore: regenerate proto classes

--- src/wa_crypt_tools/proto/key_type_pb2.py
+++ src/wa_crypt_tools/proto/key_type_pb2.py
@@ -4,7 +4,7 @@ from google.protobuf import descriptor_pool as _descriptor_pool
 from google.protobuf import runtime_version as _runtime_version
 from google.protobuf import symbol_database as _symbol_database
 from google.protobuf.internal import builder as _builder
-_runtime_version.ValidateProtobufRuntimeVersion(_runtime_version.Domain.PUBLIC, 5, 27, 3, '', 'key_type.proto')
+_runtime_version.ValidateProtobufRuntimeVersion(_runtime_version.Domain.PUBLIC, 5, 29, 1, '', 'key_type.proto')
 _sym_db = _symbol_database.Default()
 DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0ekey_type.proto*/\n\x08Key_Type\x12\x0f\n\x0bWA_PROVIDED\x10\x00\x12\x12\n\x0eHSM_CONTROLLED\x10\x01b\x06proto3')
 _globals = globals()
